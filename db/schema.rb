e8cbee17bbf25c3eae2b682e6e7d4fb3de46c97c
Redzep Demic <arrdy_90@hotmail.com>
2016-08-19 19:02:58 +0200

    budget model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818120622) do
+ActiveRecord::Schema.define(version: 20160819165658) do
+
+  create_table "budgets", force: :cascade do |t|
+    t.decimal  "value"
+    t.date     "budget_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
