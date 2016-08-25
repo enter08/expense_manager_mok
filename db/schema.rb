ab494af42870d432f5e59ad4eaf53920d424d664
Redzep Demic <arrdy_90@hotmail.com>
2016-08-25 16:09:36 +0200

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825121945) do
+ActiveRecord::Schema.define(version: 20160825140352) do
 
   create_table "budgets", force: :cascade do |t|
     t.decimal  "value"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20160825121945) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
